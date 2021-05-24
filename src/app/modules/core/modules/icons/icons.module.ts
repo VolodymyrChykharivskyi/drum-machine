b9841e73c7979b5d28d0d936460613a6d03e8ac8
Volodymyr Chykharivskyi <v.chykharivskyi@tenantcloud.com>
2021-05-24 17:04:43 +0300

    Removed unused icons

--- src/app/modules/core/modules/icons/icons.module.ts
+++ src/app/modules/core/modules/icons/icons.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import {
-	RiAncientGateFill,
-	RiHome3Line,
 	RiStopFill,
 	RiPlayFill,
 	RemixIconModule,
 } from 'angular-remix-icon';
 
 const icons = {
-	RiAncientGateFill,
-	RiHome3Line,
 	RiStopFill,
 	RiPlayFill,
 };
